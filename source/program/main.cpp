e311303cd73d0c18e605043acaf53a6afbc523a0
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2025-10-17 00:05:22 -0600

    [feat] move randomizer

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -6,37 +6,33 @@ inline static Ret external(long addr, Args... args) {
     return reinterpret_cast<Ret(*)(Args...)>(exl::util::modules::GetTargetOffset(addr))(args...);
 }
 
-struct encounter_info_t {
-    u8 unk_0[0x48];
-    u16 species;
-    u8 unk_1[0x6];
-    f32 alpha_rate;
-    u16 alpha_level_boost;
-    u64 can_be_alpha;
-    u8 unk_2[0x6];
-    u16 form;
-    // ...
-} PACKED;
+u16 rand_move() {
+    u16* move_table = reinterpret_cast<u16*>(exl::util::modules::GetTargetOffset(0x3156fd8));
+    return move_table[exl::util::GetRandomU64() % 247];
+}
 
-
-HOOK_DEFINE_INLINE(AllAlphaRandomzer) {
+HOOK_DEFINE_INLINE(MoveRandomizer) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
-        auto encounter_info = reinterpret_cast<encounter_info_t*>(ctx->X[0]);
-        encounter_info->alpha_rate = 101.0f;
-        do {
-            // TODO: this is INCREDIBLY naive
-            encounter_info->species = (exl::util::GetRandomU64() % 1025) + 1;
-            encounter_info->form = exl::util::GetRandomU64() % 31;
-            // PersonalInfo::ExistsInGame
+        static float last_timers[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+        u64 pkm = *reinterpret_cast<u64*>(*reinterpret_cast<u64*>(ctx->X[19] + 0x50) + 0x70);
+        float* timers = reinterpret_cast<float*>(ctx->X[19] + 0x5c);
+        for (int i = 0; i < 4; i++) {
+            // move was just used so its timer is reset from 0
+            if (last_timers[i] == 0.0f && timers[i] != 0.0f) {
+                // SetMove(pkm, index, move_id)
+                external<void>(0xd11e60, pkm, i, rand_move());
+            }
+            last_timers[i] = timers[i];
         }
-        while (!external<bool>(0x2aa84c, encounter_info->species, encounter_info->form));
+        
     }
 };
 
 extern "C" void exl_main(void* x0, void* x1) {
     exl::hook::Initialize();
 
-    AllAlphaRandomzer::InstallAtOffset(0x42eb34);
+    MoveRandomizer::InstallAtOffset(0xbfe318);
 }
 
 extern "C" NORETURN void exl_exception_entry() {
