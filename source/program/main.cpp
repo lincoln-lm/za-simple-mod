1b677669baf65d5ddcf5e9248b4e58bda4f9606e
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2025-10-23 22:58:27 -0600

    [feat] basic starter randomizer

--- source/program/main.cpp
+++ source/program/main.cpp
@@ -1,44 +1,50 @@
 #include "lib.hpp"
-#include <memory>
+#include <random>
 
 template <typename Ret, typename... Args>
 inline static Ret external(long addr, Args... args) {
     return reinterpret_cast<Ret(*)(Args...)>(exl::util::modules::GetTargetOffset(addr))(args...);
 }
 
-static u8 last_selected_mon = 0;
-static u8 last_experience_index = 0;
+static u64 session_seed = 0;
 
-HOOK_DEFINE_INLINE(UpdateLastSentOutMon) {
-    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
-        last_selected_mon = ctx->W[8];
-    }
-};
+struct InitSpec {
+    u64 seed;
+    u64 is_seeded;
+    u64 ec;
+    u64 tidsid;
+    u64 pid;
+    u16 shiny_rolls;
+    u16 species;
+    u16 form;
+    // ...
+} PACKED;
 
-HOOK_DEFINE_INLINE(ResetExperienceIndex) {
+HOOK_DEFINE_INLINE(RandomizeStarter) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
-        last_experience_index = 0;
-    }
-};
-
-HOOK_DEFINE_INLINE(GatekeepExperience) {
-    static void Callback(exl::hook::nx64::InlineCtx* ctx) {
-        // only give exp to the pokemon that is currently out (presumably the one that was used in battle)
-        if (last_experience_index++ != last_selected_mon) {
-            ctx->W[8] = 0;
+        auto init_spec = reinterpret_cast<InitSpec*>(ctx->X[0]);
+        if (init_spec->species == 152 || init_spec->species == 158 || init_spec->species == 498) {
+            std::mt19937_64 mt(session_seed + init_spec->species);
+            std::uniform_int_distribution<u64> dist_species(0, 1025);
+            std::uniform_int_distribution<u64> dist_form(0, 31);
+            do {
+                // TODO: this is INCREDIBLY naive
+                init_spec->species = dist_species(mt) + 1;
+                init_spec->form = dist_form(mt);
+                // PersonalInfo::ExistsInGame
+            }
+            while (!external<bool>(0x2aa84c, init_spec->species, init_spec->form));
         }
     }
 };
 
+
 extern "C" void exl_main(void* x0, void* x1) {
     exl::hook::Initialize();
+    session_seed = exl::util::GetRandomU64();
 
-    // start of function that gives exp to party
-    ResetExperienceIndex::InstallAtOffset(0x2420dc);
-    // part of the above function that reads the amount of exp to give
-    GatekeepExperience::InstallAtOffset(0x242164);
-    // called constantly in the overworld and checks the sent out mon's index
-    UpdateLastSentOutMon::InstallAtOffset(0x2e7380);
+    // FixInitSpec
+    RandomizeStarter::InstallAtOffset(0x69b6c0);
 }
 
 extern "C" NORETURN void exl_exception_entry() {
